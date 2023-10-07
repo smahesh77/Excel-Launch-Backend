82a23eb71d7f211965dbaf07b301f20463dd80e9
Mayon-Francis <mayonfrancis@gmail.com>
2023-10-07 23:46:15 +0530

    cors

--- index.ts
+++ index.ts
@@ -2,6 +2,7 @@ import express, { Request, Response } from 'express';
 import { PrismaClient } from '@prisma/client';
 import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
+import cors from 'cors';
 
 dotenv.config();
 
@@ -9,6 +10,7 @@ const prisma = new PrismaClient();
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 const jwtSecret = process.env.SECRET_KEY || ""
 
