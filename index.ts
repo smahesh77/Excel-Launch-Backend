55b6d82687c54a4687314d82b6a65d6e515d326b
smahesh77 <smahesh7736@gmail.com>
2023-10-08 10:18:54 +0530

    website/mascot status and redirecting links

--- index.ts
+++ index.ts
@@ -1,81 +1,28 @@
-import express, { Request, Response } from 'express';
-import { PrismaClient } from '@prisma/client';
-import jwt from 'jsonwebtoken';
+import express from 'express';
+import cors from 'cors';
 import dotenv from 'dotenv';
+
+import launchRoutes from './routes/launchRoute';
+import dynamicLinkRoutes from './routes/linkRoute';
 
 dotenv.config();
 
-const prisma = new PrismaClient();
-
 const app = express();
 app.use(express.json());
+app.use(cors());
 
-const jwtSecret = process.env.SECRET_KEY || ""
 
-
-// ... Other imports and setup ...
-
-app.post('/status', async (req: Request, res: Response) => {
-    try {
-      const authorizationHeader = req.headers.authorization;
-      
-      if (!authorizationHeader) {
-        return res.status(401).json({ error: 'Unauthorized: Missing authorization header' });
-      }
-  
-      const tokenParts = authorizationHeader.split(' ');
-      
-      if (tokenParts.length !== 2 || tokenParts[0] !== 'Bearer') {
-        return res.status(401).json({ error: 'Unauthorized: Invalid authorization header format' });
-      }
-  
-      const token = tokenParts[1];
-  
-      const decodedToken = jwt.verify(token, jwtSecret);
-      const role = (decodedToken as { role: string }).role;
-  
-      if (!role.includes('Admin')) {
-          return res.status(403).json({ error: 'Unauthorized: Only Admins can change logostatus' });
-      }
-  
-      const newStatus = req.body.newStatus;
-      const logoStatus = newStatus 
-  
-      await prisma.lauchStatus.upsert({
-        where: { id: 1 },
-        update: { logoStatus },
-        create: { id: 1, logoStatus },
-      });
-  
-      return res.status(201).json({ message: 'Logo status updated successfully' });
-    } catch (error) {
-      console.error('Error setting logo status:', error);
-      return res.status(500).json({ error: 'Internal server error' });
-    }
-  });
-  
-  // ... Rest of the code ...
-  
-app.get('/status', async (req: Request, res: Response) => {
-  try {
-    const logoStatus = await prisma.lauchStatus.findUnique({ where: { id: 1 } });
-
-    if (!logoStatus) {
-      return res.status(404).json({ error: 'Logo status not found' });
-    }
-
-    return res.status(200).json({ logoStatus });
-  } catch (error) {
-    console.error('Error fetching logo status:', error);
-    return res.status(500).json({ error: 'Internal server error' });
-  }
+app.get('/ping', (req: express.Request, res: express.Response) => {
+  res.status(200).json({ message: 'pong', status: 'OK' });
 });
 
-app.get("/ping", (req: Request, res: Response) => {
-  res.status(200).json(({message:"pong", status:"OK"}))
-})
+// Launch routes
+app.use('/launch', launchRoutes);
 
-app.use((err: Error, req: Request, res: Response, next: any) => {
+// Dynamic Link routes
+app.use('/links', dynamicLinkRoutes);
+
+app.use((err: Error, req: express.Request, res: express.Response, next: () => void) => {
   if (err.name === 'UnauthorizedError') {
     return res.status(401).json({ error: 'Unauthorized' });
   }
